4a4884c64f89773a5cb339a1c881d0b161e60b85
liuxin <2630484635@qq.com>
2026-06-22 16:49:03 +0800

    init: 初始化

---
+++ rag-app/src/main/java/com/hd/rag/RAGServiceApplication.java
@@ -0,0 +1,11 @@
+package com.hd.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RAGServiceApplication {
+    static void main(String[] args) {
+        SpringApplication.run(RAGServiceApplication.class, args);
+    }
+}
